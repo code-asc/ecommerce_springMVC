69f011da1d949fb285acb7859d962750a70d5dc6
sandeep chowdary <avgchowdary@gmail.com>
2017-06-05 13:00:59 +0530

    minor bug fixes

--- src/com/app/controller/SessionManagement.java
+++ src/com/app/controller/SessionManagement.java
@@ -3,8 +3,6 @@ package com.app.controller;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.app.repository.UserDetails;
 
@@ -22,7 +20,6 @@ public class SessionManagement implements HttpSessionListener
 	public void sessionDestroyed(HttpSessionEvent event)
 	{ 
 		UserDetails info = new UserDetails();
-		System.out.println((int)event.getSession().getAttribute("userID"));
 		info.removeUserStatus((int)event.getSession().getAttribute("userID"));
 	}
 	
